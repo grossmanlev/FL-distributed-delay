f7286fc877b7d2d48b4ae964fa8f79d813840c67
grossmanlev <lgrossman@college.harvard.edu>
2019-12-11 19:57:35 -0500

    more cleaning

--- FL/models.py
+++ FL/models.py
@@ -7,74 +7,6 @@ import numpy as np
 import math
 
 
-class Unit(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
-        super(Unit, self).__init__()
-
-        self.conv = nn.Conv2d(in_channels=in_channels, kernel_size=kernel_size,
-                              out_channels=out_channels, stride=stride,
-                              padding=1)
-        self.bn = nn.BatchNorm2d(num_features=out_channels)
-        self.relu = nn.ReLU()
-
-    def forward(self, input):
-        output = self.conv(input)
-        output = self.bn(output)
-        output = self.relu(output)
-        return output
-
-
-class FirstNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(FirstNet, self).__init__()
-
-        self.unit1 = Unit(in_channels=3, out_channels=32, kernel_size=5)
-        self.unit2 = Unit(in_channels=32, out_channels=32, kernel_size=4)
-        # self.unit3 = Unit(in_channels=32, out_channels=32)
-
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
-
-        self.unit4 = Unit(in_channels=32, out_channels=64)
-        # self.unit5 = Unit(in_channels=64, out_channels=64)
-        # self.unit6 = Unit(in_channels=64, out_channels=64)
-        # self.unit7 = Unit(in_channels=64, out_channels=64)
-
-        self.avgpool = nn.AvgPool2d(kernel_size=6)
-
-        self.net = nn.Sequential(self.unit1, self.unit2,
-                                 self.pool1, self.unit4,
-                                 self.avgpool)
-
-        self.fc = nn.Linear(in_features=256, out_features=num_classes)
-
-    def forward(self, input):
-        output = self.net(input)
-        # print(output.shape)
-        output = output.view(-1, 256)
-        # print(output.shape)
-        output = self.fc(output)
-        return output
-
-
-class SecondNet(nn.Module):
-    def __init__(self):
-        super(SecondNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
 # http://parneetk.github.io/blog/cnn-cifar10/
 class PerformantNet1(nn.Module):
     def __init__(self):
@@ -111,20 +43,6 @@ class PerformantNet1(nn.Module):
         x = self.dropout2(x)
         x = F.relu(self.linear2(x))
         x = self.dropout2(x)
-        x = self.linear3(x)
-        return x
-
-class AdvNet(nn.Module):
-    def __init__(self, input_dim, activation_function=F.relu):
-        super(AdvNet, self).__init__()
-        self.linear1 = nn.Linear(input_dim, 256)
-        self.linear2 = nn.Linear(256, 64)
-        self.linear3 = nn.Linear(64, 10)
-        self.activation_function = activation_function
-
-    def forward(self, x):
-        x = self.activation_function(self.linear1(x))
-        x = self.activation_function(self.linear2(x))
         x = self.linear3(x)
         return x
 
